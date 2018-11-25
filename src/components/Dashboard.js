0765b477a0756a05948733266e23d2b8b81723a5
kml1326 <kmlraj131996@gmail.com>
2018-11-25 20:15:49 +0530

    add dashboard button

---
+++ src/components/Dashboard.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Dashboard extends Component {
+  render() {
+    return (
+      <div>
+        <button className='circle'>+</button>
+      </div>
+    )
+  }
+}
+
+export default Dashboard ;
