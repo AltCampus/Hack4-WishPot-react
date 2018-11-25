1d3f0a4fb58e124bf9a76ce12bb033d899a22032
ashwanig3 <goswami17829@gmail.com>
2018-11-25 21:49:01 +0530

    actions implement

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.scss';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Dashboard from './components/Dashboard';
 import CreateWish from './components/CreateWish';
+import Wishes from './components/Wishes'
 
 class App extends Component {
   render() {
@@ -10,8 +11,9 @@ class App extends Component {
       <div className="App">
         <Router>
           <Switch>
-            <Route path='/' component={Dashboard} exact />
-            <Route path='/create' component={CreateWish} exact />
+            <Route path='/' exact component={Dashboard} exact />
+            <Route path='/create' component={CreateWish}  />
+            <Route path='/board/:id' component={Wishes}  />
           </Switch>
         </Router> 
       </div>
--- src/actions/action.js
+++ src/actions/action.js
@@ -1,8 +1,18 @@
 // action for add wish board
 
 export function addWishBoard(value){
+    console.log(value)
     return{
         type: 'ADD_WISHBOARD',
         value
+    }
+}
+
+export function addWishItem(value,id){
+    console.log(value, id)
+    return{
+        type: 'ADD_WISHITEM',
+        value,
+        id
     }
 }
