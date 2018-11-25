c1720b9d221d01e05aa4c17fc2a44d8663c9f8ee
kml1326 <kmlraj131996@gmail.com>
2018-11-25 20:22:54 +0530

    import createWish component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.scss';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Dashboard from './components/Dashboard';
+import CreateWish from './components/CreateWish';
 
 class App extends Component {
   render() {
